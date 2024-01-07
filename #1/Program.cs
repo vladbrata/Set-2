de514efce7d71b655de41208cb910c8a8c79347c
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-07 12:10:56 +0200

    Rezolvare

--- #1/Program.cs
+++ #1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Se da o secventa de n numere. Sa se determine cate din ele sunt pare.
+
+Console.WriteLine("Cate numere sunt in sirul de numere?");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] seq = GetSequence(n);
+int numerePare = GetPairNumbers(seq);
+
+Console.WriteLine("Numerele din sir: ");
+foreach (int number in seq) {
+    Console.Write(number + " ");
+}
+
+Console.WriteLine($"\nIn sirul de numere dat sunt {numerePare} numere pare!");
+
+static int[] GetSequence(int n)
+{
+    Random num = new Random();
+    int[] nums = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        nums[i] = num.Next(1, 9);
+    }
+    return nums;
+}
+static int GetPairNumbers(int[] arr)
+{
+    int numerePare = 0;
+
+    foreach (int number in arr) {
+        if (number % 2 == 0)
+            numerePare++;
+    }
+    return numerePare;
+}
