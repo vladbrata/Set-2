63a9f0dc9a75a7be19a3d8a632ccf8d689665297
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-07 12:29:10 +0200

    Solution

--- #5/Program.cs
+++ #5/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Cate elemente dintr-o secventa de n numere sunt egale cu pozitia pe care apar in secventa. Se considera ca primul element din secventa este pe pozitia 0.
+
+Console.Write("Cate numere sunt in sirul de numere? ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] seq = GetSeq(n);
+PrintSeq(seq);
+
+int cnt = GetEqualElements(seq);
+Console.WriteLine($"\n{cnt} elemente din secventa sunt egale cu pozitia pe care apar");
+
+static int[] GetSeq(int n)
+{
+    Random num = new Random();
+    int[] nums = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        nums[i] = num.Next(-9, 9);
+    }
+    return nums;
+}
+static void PrintSeq(int[] arr)
+{
+    Console.WriteLine("Numere din sir: ");
+
+    foreach (int number in arr) {
+        Console.Write(number + " ");
+    }
+}
+
+static int GetEqualElements(int[] arr)
+{
+    int cnt = 0;
+
+    for (int i = 0; i < arr.Length; i++) {
+        if (arr[i] == i) {
+            cnt++;
+        }
+    }
+    return cnt;
+}
