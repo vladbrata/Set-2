49dc26e6b3c4ddb48131a06a405389f53e073a40
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-08 12:07:27 +0200

    Solution

--- #7/Program.cs
+++ #7/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Se da o secventa de n numere. Sa se determine cea mai mare si cea mai mica valoare din secventa.
+
+Console.Write("Cate numere sunt in sirul de numere? ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] seq = GetSeq(n);
+PrintSeq(seq);
+
+int largest = seq[0];
+int smallest = seq[0];
+for (int i = 0; i < n; i++)
+{
+    if (seq[i] > largest) {
+        largest = seq[i];
+    }
+    if (seq[i] < smallest) {
+        smallest = seq[i];
+    }
+}
+Console.WriteLine($"\nCea mai mica valoare din secventa de numere este: {smallest}");
+Console.WriteLine($"Cea mai mare valoare din secventa de numere este: {largest}");
+
+static int[] GetSeq(int n)
+{
+    Random num = new Random();
+    int[] nums = new int[n];
+    
+    for (int i = 0; i < n; i++) {
+        nums[i] = num.Next(-9, 9);
+    }
+    return nums;
+}
+static void PrintSeq(int[] arr)
+{
+    Console.WriteLine("Numere din sir: ");
+
+    foreach (int number in arr) {
+        Console.Write(number + " ");
+    }
+}
