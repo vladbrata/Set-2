832afef90235c8362502b484a977d5807b7f280f
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-08 12:10:45 +0200

    Enuntul Problemei

--- #8/Program.cs
+++ #8/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Determianti al n-lea numar din sirul lui Fibonacci. Sirul lui Fibonacci se construieste astfel: f1 = 0, f2 = 1, f_n = f_(n-1) + f(n-2). Exemplu: 0, 1, 1, 2, 3, 5, 8 ...
